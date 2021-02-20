c4dc1d7a1952547b785b93c6f36a74329bb1a71f
carlos3g <carlosmesquita156@gmail.com>
2021-02-19 22:15:41 -0300

    [FEAT] configure relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+const importOptions = {
+  paths: [
+    {
+      rootPathSuffix: 'src',
+    },
+    {
+      rootPathSuffix: './src/assets',
+      rootPathPrefix: '@assets/',
+    },
+    {
+      rootPathSuffix: './src/components',
+      rootPathPrefix: '@components/',
+    },
+    {
+      rootPathSuffix: './src/screens',
+      rootPathPrefix: '@screens/',
+    },
+  ],
+};
+
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [['babel-plugin-root-import', importOptions]],
   };
 };
